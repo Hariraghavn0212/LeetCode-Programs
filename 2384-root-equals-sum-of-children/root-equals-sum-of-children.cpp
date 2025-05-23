99b2e843cf60cf2d9e679855812157ccce5afea0
Hariraghavan T <hariraghavan254@gmail.com>
2025-05-23 14:36:58 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (30.05%) - LeetSync

--- 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
@@ -12,9 +12,11 @@
 class Solution {
 public:
     bool checkTree(TreeNode* root) {
-        if(root->val == root->left->val + root->right->val)
+        if(root->val == root->left->val + root->right->val){
         return true;
+    }else{
 
         return false;
         }
+    }
 };
