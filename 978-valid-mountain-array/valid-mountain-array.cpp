7120c9eb5e8dbe3b84d8f0954df3b08a34318556
Hariraghavan T <hariraghavan254@gmail.com>
2025-06-04 14:55:44 +0530

    Time: 16 ms (77.95%) | Memory: 26.2 MB (69.99%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+
+        if(arr.size() < 3) 
+            return false;        
+        
+        int peak = 0;  
+               
+        for(int i = 1; i < arr.size(); i++) 
+        {
+            if(arr[i] > arr[i-1]) 
+                peak = i; 
+            
+             else
+                break; 
+        }
+
+        if(peak == 0 || peak == arr.size() - 1) 
+            return false;
+      
+        for(int i = peak; i < arr.size() - 1; i++) 
+        {
+            if(arr[i] <= arr[i+1]) 
+                return false; 
+        }      
+        return true;
+    }
+};
